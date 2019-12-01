5af0fac86260b7cc1d3e53d0990c217a56a0147f
Piotr Pawelczyk <piopawelczyk@gmail.com>
2019-12-01 18:54:37 +0100

    challenge 3: js file start

---
+++ js/challenge3.js
@@ -0,0 +1,9 @@
+function rpsGame(choice){
+    var humanChoice, botChoice;
+    // humanChoice = choice.id;
+    // botChoice = 
+    // results = decideWinner(humanChoice, botChoice); <0, 0.5, 1> -> Computer, Tie, Human won
+    // message = finalMessage(results); // object(dict in python) {message: "", color: ""}
+    rpsFrontend(choice, botChoice, message);
+}
+
