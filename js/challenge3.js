d32767d4f968b467d07ad1e3a8689ccc3f7ce7d7
Piotr Pawelczyk <piopawelczyk@gmail.com>
2019-12-02 12:08:26 +0100

    challenge 3: added elements removal

--- js/challenge3.js
+++ js/challenge3.js
@@ -12,6 +12,8 @@ function rpsGame(choiceDiv){
         results = decideWinner(humanChoice, botChoice); //<-1, 0, 1> -> Computer, Tie, Human won
         message = finalMessage(results); // object(dict in python) {message: "", color: ""}
         rpsFrontend(humanChoice, botChoice, message);
+    } else {
+        loadImages();
     }
 }
 
@@ -43,26 +45,38 @@ function finalMessage(results){
 }
 
 function rpsFrontend(humanChoice, botChoice, message){
-     removeImages();
+     removeElements();
      rpsDiv1 = document.getElementById('rps-div-1');
      rpsDiv2 = document.getElementById('rps-div-2');
      rpsDiv3 = document.getElementById('rps-div-3');
+     rpsDiv1.classList.remove("choice-box");
+     rpsDiv2.classList.remove("choice-box");
+     rpsDiv3.classList.remove("choice-box");
     // add new elements to the divs
     imgDiv1 = document.createElement("img");
     imgDiv1.src = imageDb[humanChoice];
     imgDiv3 = document.createElement("img");
     imgDiv3.src = imageDb[botChoice];
+
     messageText = document.createElement("h2");
     messageText.innerHTML = message["message"];
     messageText.style.color = message["color"];
+    returnButton = document.createElement("p");
+    returnButton.innerHTML = "Play once again";
+    returnButton.classList.add("button", "run-fn");
+    returnButton.id = "rps-return"
 
     rpsDiv1.appendChild(imgDiv1);
     rpsDiv2.appendChild(messageText);
+    rpsDiv2.appendChild(returnButton);
     rpsDiv3.appendChild(imgDiv3);
 }
 
 function loadImages(){
     // Run on pagecreate or on refresh of the game
+    
+    // first remove all the elements from divs (if there are any)
+    removeElements();
     rpsDiv1 = document.getElementById('rps-div-1');
     rpsDiv2 = document.getElementById('rps-div-2');
     rpsDiv3 = document.getElementById('rps-div-3');
@@ -86,9 +100,14 @@ function loadImages(){
     rpsDiv3.classList.add("choice-box");
 }
 
-function removeImages(){
-    document.getElementById("rock").remove();
-    document.getElementById("paper").remove();
-    document.getElementById("scissors").remove();
+function removeElements(){
+    rpsContainer = document.getElementById("rps-container");
+    rpsContainer.childNodes.forEach(removeChildNodes);
+}
+
+function removeChildNodes(element){
+    while (element.lastChild){
+        element.removeChild(element.lastChild);
+    }
 }
 
