2d0242479b13a15daba0e8a5ca58c1463b093107
Piotr Pawelczyk <piopawelczyk@gmail.com>
2019-12-03 20:50:02 +0100

    Ajax1 basis: full

--- ajaxcrash/js/ajax1.js
+++ ajaxcrash/js/ajax1.js
@@ -11,10 +11,39 @@ function loadText(){
     // OPEN(type, url/file, async)
     xhr.open('GET', "./otherFiles/sample.txt", true);
     //  it is considering the home directory as the directory of the HTML!
+    console.log("Readystate: ", xhr.readyState);
+
+    // OPTIONAL (before onload) -> used for loaders (those rotating circles or whatever when page is loading)
+    xhr.onprogress = function(){
+        console.log("Readystate: ", xhr.readyState); // == 3
+    }
     xhr.onload = function(){
+        console.log("Readystate: ", xhr.readyState); // the readyState is already 4
         if(this.status == 200){ // check if it is ok before you do anything else
-            console.log(this.responseText);
+            // console.log(this.responseText);
+            document.querySelector("#text").innerHTML = this.responseText;
+        } else if (this.status == 404){
+            document.querySelector("#text").innerHTML = "Not found"
         }
     }
+
+    xhr.onerror = function(){
+        console.log("Request error :(");
+    }
+
+
+    // an old way instead of the onload function, requires checking of the readyState
+    // xhr.onreadystatechange = function(){
+    //     console.log("Readystate: ", xhr.readyState);
+    //     // so in this old way, it goes through all the checks of readyState: (1. 2. 3. 4)
+    //     // 0: request not initialized 
+    //     // 1: server connection established
+    //     // 2: request received 
+    //     // 3: processing request 
+    //     // 4: request finished and response is ready
+    //     if(this.readyState == 4 && this.status == 200){ // readyState is a property of xhr
+    //         // perform your function
+    //     }
+    // }
     xhr.send();
 }
